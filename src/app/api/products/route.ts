d0b40eed38f6a6c1f44ca9d2f885ab0e1d5e5902
Ranga McMil <ranga@Rangas-MacBook-Air.local>
2024-04-08 11:10:16 +0200

    debouncing

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,11 +1,75 @@
-import { db } from "@/db"
+import { db } from "@/db";
+import { ProductFilterValidator } from "@/lib/validators/product-validator";
+import { NextRequest } from "next/server";
 
-export const POST = async () => {
-    const products = await db.query({
-        topK: 12,
-        vector: [0, 0, 0],
-        includeMetadata: true
-    })
+const AVG_PRODUCT_PRICE = 25;
+const MAX_PRODUCT_PRICE = 50;
 
-    return new Response(JSON.stringify(products))
+class Filter {
+  private filters: Map<string, string[]> = new Map();
+
+  hasFilter() {
+    return this.filters.size > 0;
+  }
+
+  add(key: string, operator: string, value: string | number) {
+    const filter = this.filters.get(key) || [];
+
+    // eg (color = value)
+    filter.push(
+      `${key} ${operator} ${typeof value === "number" ? value : `"${value}"`}`
+    );
+    this.filters.set(key, filter);
+  }
+
+  addRaw(key: string, rawFilter: string) {
+    this.filters.set(key, [rawFilter]);
+  }
+
+  get() {
+    const parts: string[] = [];
+    this.filters.forEach((filter) => {
+      const groupedValues = filter.join(` OR `);
+      parts.push(`(${groupedValues})`);
+    });
+    return parts.join(" AND ");
+  }
 }
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+
+    const { color, price, size, sort } = ProductFilterValidator.parse(
+      body.filter
+    );
+
+    const filter = new Filter();
+
+    color.forEach((color) => filter.add("color", "=", color));
+    size.forEach((size) => filter.add("size", "=", size));
+    filter.addRaw("price", `price >= ${price[0]} AND price <= ${price[1]}`);
+
+    const products = await db.query({
+      topK: 12,
+      vector: [
+        0,
+        0,
+        sort === "none"
+          ? AVG_PRODUCT_PRICE
+          : sort === "price-asc"
+          ? 0
+          : MAX_PRODUCT_PRICE,
+      ],
+      includeMetadata: true,
+      filter: filter.hasFilter() ? filter.get() : undefined,
+    });
+
+    return new Response(JSON.stringify(products));
+  } catch (error) {
+    console.error(error)
+    return new Response(JSON.stringify({message: "Internal Server Error"}), {
+        status: 500
+    })
+  }
+};
--- src/components/Products/Product.tsx
+++ src/components/Products/Product.tsx
@@ -10,6 +10,19 @@ const Product = ({product}: {product: Product}) => {
                     className="h-full w-full object-cover object-center"
                 />
             </div>
+            <div className="mt-4 flex justify-between">
+                <div>
+                    <h3 className="text-sm text-gray-700">
+                        {product.name}
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                        Size {product.size.toUpperCase()}, {product.color}
+                    </p>
+                </div>
+
+                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+
+            </div>
         </div>
     )
 }
